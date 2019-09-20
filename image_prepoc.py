781dd5a21e12cb01f91d913d2f2a278b22acfc64
Alexander Piers <apiers@uw.edu>
2019-09-19 17:21:35 -0700

    add - class for analysis information storage and input/output file support

--- image_prepoc.py
+++ image_prepoc.py
@@ -2,6 +2,56 @@ import argparse
 import regex as re
 import sys
 import os
+import inspect
+
+sys.path.append("AutoAnalysis")
+
+
+class analysisOutput(object):
+    """Class that contains the information on the output results of the image analysis"""
+    def __init__(self, filename, noise=-1, atest=-1, header=[]):
+        super(analysisOutput, self).__init__()
+
+        self.noise = noise
+        self.atest = atest
+
+        self.filename = filename
+        self.header = list(header)
+        self.headerString = ""
+
+        # Create a header string for writing out to file
+        self.headerString = self.filename + "\t"
+        for s in self.header:
+            self.headerString += str(s) + "\t"
+        self.headerString += "\n"
+
+    def __str__(self):
+        """ Prints information regarding the analysis of an image """
+        # Define output string
+        outstr = "Analysis of " + self.filename + "\n"
+
+        # Get all the user defined variables that contain information on the image analysis
+        classAttributesAndMethods = inspect.getmembers(self, lambda a : not(inspect.isroutine(a)))
+        classAnalysisAttributes = [a for a in classAttributesAndMethods if not(a[0].startswith("__") and a[0].endswith("__") or("header" in a[0]) or (a[0] == "filename"))]
+        
+        # Print the current value of all the analysis variables
+        for analysisVar in classAnalysisAttributes:
+            outstr += "\t"
+            outstr += analysisVar[0] + ": " + str(analysisVar[1])
+            outstr += "\n"
+        return outstr
+
+    def getTableString(self):
+        """ 
+            Returns a string of the class values depending on what values we want to return specified in the header attribute 
+            For printing values in tabulated format.
+        """
+        outstr = self.filename + "\t"
+        for outVar in self.header:
+            outstr += str(self.__getattribute__(outVar))
+            outstr += "\t"
+        outstr += "\n"
+        return outstr
 
 
 def main(argv):
@@ -42,6 +92,7 @@ def main(argv):
 
     # Get a list of files in the directory
     filepath = commandArgs.directory
+    outfile = commandArgs.output
     files = [
         f for f in os.listdir(filepath) if os.path.isfile(os.path.join(filepath, f))
     ]
@@ -53,12 +104,42 @@ def main(argv):
 		# Define regex matching
     	regexPattern = re.compile(fn)
     	files2Process.extend(list(filter(regexPattern.match, files)))
-    
-
     files2Process.sort()
+
+    # Check what images have already been analyzed and written to file so we do not need to recompute
+    try:
+        with open(outfile) as of:
+            print("Reading existing processed images")
+            existingImgFiles =  [line.split()[0] for line in of]
+    except FileNotFoundError:
+        existingImgFiles = []
+
+    # Process images with functions necessary to characterize the quality of images
+    processedImgFiles = []
+    processHeader = []
     print("Processing: ")
     for fp in files2Process:
-        print("\t" + os.path.join(filepath, fp))
+        # Check to make sure the file has not already been processed
+        if not(fp in existingImgFiles):
+            print("\t" + os.path.join(filepath, fp))
+            processedImgFiles.append(analysisOutput(fp, header=processHeader))
+
+
+    # Appends new images to the output file or creates new file if doesn't exist
+    if os.path.isfile(outfile):
+        of = open(outfile, "a")
+    else:
+        of = open(outfile, "w+")
+        try:
+            of.write(processedImgFiles[0].headerString)
+        except IndexError:
+            pass
+
+    # Write all the processed data
+    for processedImg in processedImgFiles:
+        of.write(processedImg.getTableString())
+    of.close()
+
 
     return
 
