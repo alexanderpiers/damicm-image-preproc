1c868e267e9ac4bbd731753c7f3e81e8626e6146
Alexander Piers <apiers@uw.edu>
2019-10-12 20:54:07 -0700

    update - tail distribution function to compute on all pixel values instead of the variance

--- AutoAnalysis/PixelDistribution.py
+++ AutoAnalysis/PixelDistribution.py
@@ -169,49 +169,39 @@ def computeSkImageNoise(image, nMovingAverage=10):
     return skImageNoise, skImageNoiseErr
 
 
-def computeClusterVarianceRatio(image, npixels=100, ntrials=10000):
+def computeImageTailRatio(image, nsigma=4.):
     """
-	Calculates the excess in the tail of the cluster variance (variance of ) over a gaussian fit to the peak.
-	Ratio of (number of events in the tail) / (integral of the tail) should be a proxy for clusters
+	Calculates the ratio of the number of pixels in the left tail of the distribution to the number expected if it was
+	just gaussian noise
 	Inputs:
-		image - CCD image (nrows, ncols, [nskips]) numpy array
-		npixels - size of the cluster to take when creating the cluster variance distribution
+		image - (nrows, ncols, [nskips]) numpy array. Should be raw images and not the combined image
+		nsigma - double, threshold definition of the tail
 	Outputs:
 		tailRatio - double, ratio of actual to expected number of points in the tail of the variance distribution. >> 1 is a proxy for tracks
 
 	"""
 
-    clusterVarMed, clusterVarDistribution = ps.imageNoiseVariance(
-        image, npixels, ntrials=ntrials
-    )
-    clusterVarMad = scipy.stats.median_absolute_deviation(
-        clusterVarDistribution, axis=None
-    )
-
-    # Histogram the distribution around the central point. Fit to this central distribution
-    bins = np.linspace(
-        clusterVarMed - 5 * clusterVarMad, clusterVarMed + 5 * clusterVarMad, 200
-    )
-    clusterH, clusterBinEdges = np.histogram(clusterVarDistribution, bins=bins)
-    clusterBinCenters = clusterBinEdges[:-1] + np.diff(clusterBinEdges)[0] / 2
+    imageH, imageBinCenters, imageBinEdges = histogramImage(image, nsigma=nsigma)
 
     # Fit cluster variance to a gaussian
     gausfunc = lambda x, *p: p[0] * np.exp(-(x - p[1]) ** 2 / (2 * p[2] ** 2))
+    sigmaGuess = (imageBinEdges[-1] - imageBinEdges[0]) / (2 * nsigma)
     paramGuess = [
-        len(clusterVarDistribution) / np.sqrt(2 * np.pi * clusterVarMad ** 2),
-        clusterVarMed,
-        clusterVarMad,
+        np.sum(imageH) / np.sqrt(2 * np.pi * sigmaGuess ** 2),
+        imageBinCenters[imageBinCenters.size // 2],
+        sigmaGuess,
     ]
     paramOpt, paramCov = optimize.curve_fit(
-        gausfunc, clusterBinCenters, clusterH, p0=paramGuess
+        gausfunc, imageBinCenters, imageH, p0=paramGuess
     )
 
     # Compute the ratio between the tails of the fit and data
-    tailLoc = paramOpt[1] + 3 * paramOpt[2]
-    tailRatio = np.sum(clusterVarDistribution > tailLoc) / (
-        ntrials
-        * (1 - scipy.stats.norm.cdf(tailLoc, loc=paramOpt[1], scale=paramOpt[2]))
+    tailLoc = paramOpt[1] - nsigma * paramOpt[2]
+    tailRatio = np.sum(image < tailLoc) / (
+        image.size
+        * (scipy.stats.norm.cdf(tailLoc, loc=paramOpt[1], scale=paramOpt[2]))
     )
+
     return tailRatio
 
 
@@ -253,7 +243,7 @@ def histogramImage(image, nsigma=3, minRange=None):
 
 if __name__ == "__main__":
 
-    filename = "../Img_11.fits"
+    filename = "../Img_10.fits"
 
     _, data = readFits.read(filename)
 
@@ -268,10 +258,8 @@ if __name__ == "__main__":
     # print(computeImageNoise(data[:,:,:-1], maxNskips=25))
 
     # Test cluster variance distribution
-    # for i in range(4):
-    # 	testdata = np.random.normal(10000, 60, (data.shape))
-    # 	computeClusterVarianceExcess(testdata, npixels=100, ntrials=int(10e4))
-    # # plt.show()
+    print(computeImageTailRatio(data[:,:,:-1]))
+    print(computeImageTailRatio(np.random.normal(10000, 60, data.shape)))
 
-    sigma = computeSkImageNoise(data[:, :, -1], nMovingAverage=5)
-    plt.show()
+    # sigma = computeSkImageNoise(data[:, :, -1], nMovingAverage=5)
+    # plt.show()
